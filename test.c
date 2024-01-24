68c054d2dfd48e16ac24eacdbdbad8bb29e477cf
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-24 22:13:00 +0545

    print values of static variable

--- test.c
+++ test.c
@@ -1,5 +1,18 @@
   
-        void main()   
-        {  
-            printf("Welcome to JavaTpoint");  
-        }
+        // void main()   
+        // {  
+        //     printf("Welcome to JavaTpoint");  
+        // }
+
+        // C program to print initial 
+// value of static variable 
+    #include <stdio.h> 
+    int main() 
+    { 
+        static int var; 
+        int x; 
+        printf("Initial value of static variable %d\n", var); 
+        printf("Initial value of variable without static %d", 
+            x); 
+        return 0; 
+    }
