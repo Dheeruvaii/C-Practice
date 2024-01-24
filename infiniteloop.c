9cf039dd7e65a0034d86e159ce38dc2a08eab294
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-24 22:16:37 +0545

    infiniteloop find out in c

---
+++ infiniteloop.c
@@ -0,0 +1,21 @@
+// C program for infinite loop 
+// using for, while, do-while 
+#include <stdio.h> 
+  
+// Driver code 
+int main() 
+{ 
+    for (;;) { 
+        printf("Infinite-loop\n"); 
+    } 
+  
+    while (1) { 
+        printf("Infinite-loop\n"); 
+    } 
+  
+    do { 
+        printf("Infinite-loop\n"); 
+    } while (1); 
+  
+    return 0; 
+}
